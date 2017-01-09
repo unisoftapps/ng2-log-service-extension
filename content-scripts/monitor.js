cdf2e2ffde572e5955d9f718a00e70513b8f9a90
Brian Teryek <bteryek@sufs.org>
2017-01-09 14:47:01 -0500

    Updates

--- content-scripts/monitor.js
+++ content-scripts/monitor.js
@@ -1,27 +1,7 @@
-//console.info('monitoring...');
-
-
 window.addEventListener('logMonitorEvent', function(e) {
-
-    console.log('got it!', e);
     var port = chrome.runtime.connect({name: 'ng2-log-service-monitor'});
     port.postMessage({
         type: 'logMonitorEvent',
         event: e.detail
     });
-});
-
-
-window.addEventListener('debugLauncher', function(e) {
-
-    var port = chrome.runtime.connect({name: 'ng2-log-service-monitor'});
-    port.postMessage(e.detail);
-
-    //console.log(e);
-});
-
-chrome.extension.onMessage.addListener(function (message, sender) {
-    //console.log("In content Script Message Recieved is " + message);
-    //Send needed information to background page
-    //chrome.extension.sendMessage("My URL is" + window.location.origin);
 });
